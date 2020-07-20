dd843da630450148992f00ce09644c6e96adedf9
Ryan Khalil <ryan.z.khalil@gmail.com>
2020-07-20 12:01:57 -0500

    use separate test config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+    globals: {
+        'ts-jest': {'tsConfigFile': '<rootDir>/tsconfig.test.json'}
+    },
     projects: ['<rootDir>/jest.unit.config.js', '<rootDir>/tests/jest.component.config.js'],
     testRunner: 'jest-circus/runner',
 };
